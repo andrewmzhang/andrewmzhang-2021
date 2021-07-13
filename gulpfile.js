d3666f5e6e5f19d8662647e3920185fc39179318
Andrew M. Zhang <andrewmzhang@berkeley.edu>
2021-07-12 19:02:23 -0700

    Updated password, updated hint

--- gulpfile.js
+++ gulpfile.js
@@ -91,7 +91,7 @@ function encrypt(password) {
 
 gulp.task('firewall:encrypt', () => {
   return gulp.src('_protected/*.*')
-    .pipe(encrypt('password'))
+    .pipe(encrypt('supernodesecurities'))
     .pipe(rename({extname: '.md.priv'}))
     .pipe(gulp.dest('_posts'));
 });
